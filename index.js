06d9381bf8e0fa245857820f2b1ee84f3e03f51b
Erin Sullivan <kittylily@gmail.com>
2016-10-13 23:40:03 -0700

    🐦 Post to twitter

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const tracery = require('tracery-grammar');
+const Tracery = require('tracery-grammar');
+const Twitter = require('twitter');
+const config = require('./config.json')
 const json = require('./grammar.json');
 
-let grammar = tracery.createGrammar(json);
+let grammar = Tracery.createGrammar(json);
+grammar.addModifiers(Tracery.baseEngModifiers);
 
-grammar.addModifiers(tracery.baseEngModifiers);
-
-console.log(grammar.flatten('#origin#'));
+let client = new Twitter(config);
+let status = grammar.flatten('#origin#');
+client.post('statuses/update', { status }, (error, tweet, response) => {
+  if(error) throw error;
+  console.log(tweet);
+  console.log(response);
+});
